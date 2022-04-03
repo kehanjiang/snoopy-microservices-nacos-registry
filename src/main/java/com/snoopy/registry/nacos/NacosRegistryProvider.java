d29e6f7784aee744faf99db97766854e5c0e8bdf
kehanjiang <1010805072@qq.com>
2022-04-03 22:38:46 +0800

    init

---
+++ src/main/java/com/snoopy/registry/nacos/NacosRegistryProvider.java
@@ -0,0 +1,29 @@
+package com.snoopy.registry.nacos;
+
+import com.snoopy.grpc.base.configure.GrpcRegistryProperties;
+import com.snoopy.grpc.base.registry.IRegistry;
+import com.snoopy.grpc.base.registry.IRegistryProvider;
+import com.snoopy.grpc.base.registry.nacos.NacosRegistry;
+
+/**
+ * @author :   kehanjiang
+ * @date :   2021/12/1  15:44
+ */
+public class NacosRegistryProvider implements IRegistryProvider {
+    public static final String REGISTRY_PROTOCOL_NACOS = "nacos";
+    private GrpcRegistryProperties grpcRegistryProperties;
+
+    public NacosRegistryProvider(GrpcRegistryProperties grpcRegistryProperties) {
+        this.grpcRegistryProperties = grpcRegistryProperties;
+    }
+
+    @Override
+    public IRegistry newRegistryInstance() {
+        return new NacosRegistry(grpcRegistryProperties);
+    }
+
+    @Override
+    public String registryType() {
+        return REGISTRY_PROTOCOL_NACOS;
+    }
+}
