34eb5d861418dcf464a0516fd2e070a9c3329834
kehanjiang <1010805072@qq.com>
2022-04-05 17:35:57 +0800

    fix:完善

--- src/main/java/com/snoopy/registry/nacos/NacosRegistry.java
+++ src/main/java/com/snoopy/registry/nacos/NacosRegistry.java
@@ -1,38 +1,119 @@
 package com.snoopy.registry.nacos;
 
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.listener.Event;
+import com.alibaba.nacos.api.naming.listener.EventListener;
+import com.alibaba.nacos.api.naming.listener.NamingEvent;
+import com.alibaba.nacos.api.naming.pojo.Instance;
 import com.snoopy.grpc.base.configure.GrpcRegistryProperties;
 import com.snoopy.grpc.base.registry.IRegistry;
 import com.snoopy.grpc.base.registry.ISubscribeCallback;
 import com.snoopy.grpc.base.registry.RegistryServiceInfo;
+import com.snoopy.grpc.base.utils.LoggerBaseUtil;
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.Collectors;
 
 /**
  * @author :   kehanjiang
  * @date :   2021/12/1  15:18
  */
 public class NacosRegistry implements IRegistry {
+    private static final long DEFAULT_TIMEOUT = 5_000;
+    private long timeout;
+    private NamingService namingService;
+    private EventListener eventListener;
+    private final ReentrantLock reentrantLock = new ReentrantLock();
 
-    public NacosRegistry(GrpcRegistryProperties grpcRegistryProperties) {
+    public NacosRegistry(GrpcRegistryProperties grpcRegistryProperties, Properties properties) {
+        timeout = NumberUtils.toLong(grpcRegistryProperties.getExtra(NacosRegistryProvider.PARAM_TIMEOUT));
+        timeout = timeout < 1 ? DEFAULT_TIMEOUT : timeout;
+        try {
+            namingService = NacosFactory.createNamingService(properties);
+        } catch (Exception e) {
+            LoggerBaseUtil.error(this, e.getMessage(), e);
+        }
     }
 
 
     @Override
     public void subscribe(RegistryServiceInfo serviceInfo, ISubscribeCallback subscribeCallback) {
-
+        reentrantLock.lock();
+        try {
+            this.eventListener = new EventListener() {
+                @Override
+                public void onEvent(Event event) {
+                    if (event instanceof NamingEvent) {
+                        NamingEvent namingEvent = (NamingEvent) event;
+                        List<Instance> instanceList = namingEvent.getInstances();
+                        List<RegistryServiceInfo> serviceInfoList = instanceList != null ? instanceList.stream().map(instance -> {
+                            return new RegistryServiceInfo(
+                                    serviceInfo.getNamespace(),
+                                    instance.getServiceName(),
+                                    NacosRegistryProvider.REGISTRY_PROTOCOL_NACOS,
+                                    instance.getIp(),
+                                    instance.getPort(),
+                                    instance.getMetadata());
+                        }).collect(Collectors.toList()) : Collections.EMPTY_LIST;
+                        subscribeCallback.handle(serviceInfoList);
+                    }
+                }
+            };
+            namingService.subscribe(serviceInfo.getAlias(), serviceInfo.getNamespace(), this.eventListener);
+        } catch (NacosException e) {
+            LoggerBaseUtil.error(this, "[" + serviceInfo.getPath() + "] subscribe failed !", e);
+        } finally {
+            reentrantLock.unlock();
+        }
     }
 
     @Override
     public void unsubscribe(RegistryServiceInfo serviceInfo) {
-
+        reentrantLock.lock();
+        try {
+            namingService.unsubscribe(serviceInfo.getAlias(), serviceInfo.getNamespace(), this.eventListener);
+        } catch (NacosException e) {
+            LoggerBaseUtil.error(this, "[" + serviceInfo.getPath() + "] unsubscribe failed !", e);
+        } finally {
+            reentrantLock.unlock();
+        }
     }
 
     @Override
     public void register(RegistryServiceInfo serviceInfo) {
-
+        reentrantLock.lock();
+        try {
+            Instance instance = new Instance();
+            instance.setServiceName(serviceInfo.getAlias());
+            instance.setIp(serviceInfo.getHost());
+            instance.setPort(serviceInfo.getPort());
+            instance.setMetadata(serviceInfo.getParameters());
+            instance.setHealthy(true);
+            namingService.registerInstance(serviceInfo.getPath(), serviceInfo.getNamespace(), instance);
+        } catch (NacosException e) {
+            LoggerBaseUtil.error(this, "[" + serviceInfo.getPath() + "] register failed !", e);
+        } finally {
+            reentrantLock.unlock();
+        }
     }
+
 
     @Override
     public void unregister(RegistryServiceInfo serviceInfo) {
-
+        reentrantLock.lock();
+        try {
+            namingService.deregisterInstance(serviceInfo.getAlias(), serviceInfo.getNamespace(), serviceInfo.getHost(), serviceInfo.getPort());
+        } catch (NacosException e) {
+            LoggerBaseUtil.error(this, "[" + serviceInfo.getPath() + "] unregister failed !", e);
+        } finally {
+            reentrantLock.unlock();
+        }
     }
 
 }
--- src/main/java/com/snoopy/registry/nacos/NacosRegistryProvider.java
+++ src/main/java/com/snoopy/registry/nacos/NacosRegistryProvider.java
@@ -1,8 +1,14 @@
 package com.snoopy.registry.nacos;
 
 import com.snoopy.grpc.base.configure.GrpcRegistryProperties;
+import com.snoopy.grpc.base.constans.GrpcConstants;
 import com.snoopy.grpc.base.registry.IRegistry;
 import com.snoopy.grpc.base.registry.IRegistryProvider;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * @author :   kehanjiang
@@ -10,10 +16,52 @@ import com.snoopy.grpc.base.registry.IRegistryProvider;
  */
 public class NacosRegistryProvider implements IRegistryProvider {
     public static final String REGISTRY_PROTOCOL_NACOS = "nacos";
+    public static final String PARAM_TIMEOUT = "timeout";
+    private static final Map<String, String> params = new HashMap<>();
+
+    static {
+        //---------------------------------
+        //通用参数
+        //命名空间的ID	命名空间的ID	config模块为空，naming模块为public	>= 0.8.0
+        params.put("namespace", "namespace");
+        //客户端日志的目录	目录路径	用户根目录	>= 0.1.0
+        params.put("loggingPath", "nacos.logging.path");
+        //---------------------------------
+
+        //-------------------------------------
+        //Naming客户端
+        //启动时是否优先读取本地缓存	true/false	false	>= 1.0.0
+        params.put("namingLoadCacheAtStart", "namingLoadCacheAtStart");
+        //客户端心跳的线程池大小	正整数	机器的CPU数的一半	>= 1.0.0
+        params.put("namingClientBeatThreadCount", "namingClientBeatThreadCount");
+        //客户端定时轮询数据更新的线程池大小	正整数	机器的CPU数的一半	>= 1.0.0
+        params.put("namingPollingThreadCount", "namingPollingThreadCount");
+        // (-D)	客户端缓存目录	目录路径	{user.home}/nacos/naming 	>= 1.0.0
+        params.put("namingCacheDir", "com.alibaba.naming.cache.dir");
+        //(-D)	Naming客户端的日志级别	info,error,warn等	info	>= 1.0.0
+        params.put("namingLogLevel", "com.alibaba.nacos.naming.log.level");
+        //(-D)	是否打开HTTPS	true/false	false	>= 1.0.0
+        params.put("namingTlsEnable", "com.alibaba.nacos.client.naming.tls.enable");
+        //--------------------------
+        params.put(PARAM_TIMEOUT, PARAM_TIMEOUT);
+    }
 
     @Override
     public IRegistry newRegistryInstance(GrpcRegistryProperties grpcRegistryProperties) {
-        return new NacosRegistry(grpcRegistryProperties);
+        Properties properties = new Properties();
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            String pval = grpcRegistryProperties.getExtra(entry.getKey());
+            if (!StringUtils.isEmpty(pval)) {
+                properties.setProperty(entry.getValue(), pval);
+            }
+        }
+        String serverAddr = String.join(",", GrpcConstants.ADDRESS_SPLIT_PATTERN.split(grpcRegistryProperties.getAddress()));
+        properties.setProperty("serverAddr", serverAddr);
+        if (grpcRegistryProperties.getUsername() != null && grpcRegistryProperties.getPassword() != null) {
+            properties.setProperty("username", grpcRegistryProperties.getUsername());
+            properties.setProperty("password", grpcRegistryProperties.getPassword());
+        }
+        return new NacosRegistry(grpcRegistryProperties, properties);
     }
 
     @Override
